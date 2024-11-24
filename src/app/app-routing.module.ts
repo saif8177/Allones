1f96380aee408bc5c7f0fc8cd7edc467c9ed8671
saif8177 <saipannaikwadi8177@gmail.com>
2024-11-23 22:07:47 -0800

    welcome done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,19 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
+import { FlashScreenComponent } from './flash-screen/flash-screen.component';
 const routes: Routes = [
+  {
+    path: 'flashscreen',
+    component: FlashScreenComponent // Load FlashScreenComponent directly
+  },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'flashscreen',
     pathMatch: 'full'
   },
   { path: 'signup', component: SignupComponent },
